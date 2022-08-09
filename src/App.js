5a208a912b70b90f49afac169dae39d803c8356d
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-08-09 13:46:39 -0400

    added rect router to App.js

--- src/App.js
+++ src/App.js
@@ -1,99 +1,15 @@
-import { useState } from "react";
-import Storefront from "@nacelle/storefront-sdk";
+import { Routes, Route, Link } from "react-router-dom";
 import "./styles.css";
-
-/* compat connector: */
-import NacelleClient from '@nacelle/client-js-sdk';
-import createCompatibilityConnector from "@nacelle/compatibility-connector";
+import ContentByHandleV2SDK from "./pages/ContentByHandleV2SDK";
+import Test from "./pages/Test";
 
 function App() {
-  const [spaceId, setSpaceId] = useState("");
-  const [token, setToken] = useState("");
-  const [handle, setHandle] = useState("");
-  const [nacelleEntryId, setNacelleEntryId] = useState("");
-  const [jsonResponse, setJsonResponse] = useState({
-    msg: "waiting on credentials",
-  });
-
-  async function getContent() {
-    const compatibilityConnector = new createCompatibilityConnector({
-      endpoint: `https://storefront.api.nacelle.com/graphql/v1/spaces/${spaceId}`,
-      token: token,
-      locale: "en-US",
-    });
-
-    const client = new NacelleClient({
-      connector: compatibilityConnector,
-    });
-
-    /*
-    const client = Storefront({
-      storefrontEndpoint: `https://storefront.api.nacelle.com/graphql/v1/spaces/${spaceId}`,
-      token: token,
-    });
-    */
-
-    const content = await client.data.content({
-      handles: [handle],
-    });
-    
-    setJsonResponse(content[0]);
-  }
-
   return (
-    <div className="App">
-      <h1>[V2/SDK] Get Content by Handle</h1>
-      <section>
-        <h2>Settings</h2>
-        <label htmlFor="spaceId">spaceId</label>
-        <input
-          type="text"
-          id="spaceId"
-          name="spaceId"
-          value={spaceId}
-          onChange={(e) => setSpaceId(e.target.value)}
-        />
-        <label htmlFor="token">token</label>
-        <input
-          type="text"
-          id="token"
-          name="token"
-          value={token}
-          onChange={(e) => setToken(e.target.value)}
-        />
-        <label htmlFor="handle">handle</label>
-        <input
-          type="text"
-          id="handle"
-          name="handle"
-          value={handle}
-          onChange={(e) => setHandle(e.target.value)}
-        />
-        <label htmlFor="nacelleEntryId">nacelleEntryId (do not use)</label>
-        <input
-          type="text"
-          id="nacelleEntryId"
-          name="nacelleEntryId"
-          value={nacelleEntryId}
-          onChange={(e) => setNacelleEntryId(e.target.value)}
-        />
-        <button onClick={getContent}>Submit</button>
-      </section>
-
-      <section>
-        <h2>Response</h2>
-        <button
-          onClick={() => {
-            navigator.clipboard.writeText(
-              JSON.stringify(jsonResponse, null, 2)
-            );
-          }}
-        >
-          Copy JSON
-        </button>
-        <pre>{JSON.stringify(jsonResponse, null, 2)}</pre>
-      </section>
-    </div>
+    <>
+      <Routes>
+        <Route path="/v2/content-by-handle" element={<ContentByHandleV2SDK />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/ContentByHandleV2SDK.js
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import Storefront from "@nacelle/storefront-sdk";
+
+function ContentByHandleV2SDK() {
+    const [spaceId, setSpaceId] = useState("");
+    const [token, setToken] = useState("");
+    const [handle, setHandle] = useState("");
+    const [nacelleEntryId, setNacelleEntryId] = useState("");
+    const [jsonResponse, setJsonResponse] = useState({
+      msg: "waiting on credentials",
+    });
+  
+    async function getContent() {
+      const client = Storefront({
+        storefrontEndpoint: `https://storefront.api.nacelle.com/graphql/v1/spaces/${spaceId}`,
+        token: token,
+      });
+  
+      const content = await client.content({
+        handles: [handle],
+      });
+      
+      setJsonResponse(content[0]);
+    }
+  
+    return (
+      <div className="App">
+        <h1>[V2/SDK] Get Content by Handle</h1>
+        <section>
+          <h2>Settings</h2>
+          <label htmlFor="spaceId">spaceId</label>
+          <input
+            type="text"
+            id="spaceId"
+            name="spaceId"
+            value={spaceId}
+            onChange={(e) => setSpaceId(e.target.value)}
+          />
+          <label htmlFor="token">token</label>
+          <input
+            type="text"
+            id="token"
+            name="token"
+            value={token}
+            onChange={(e) => setToken(e.target.value)}
+          />
+          <label htmlFor="handle">handle</label>
+          <input
+            type="text"
+            id="handle"
+            name="handle"
+            value={handle}
+            onChange={(e) => setHandle(e.target.value)}
+          />
+          <label htmlFor="nacelleEntryId">nacelleEntryId (do not use)</label>
+          <input
+            type="text"
+            id="nacelleEntryId"
+            name="nacelleEntryId"
+            value={nacelleEntryId}
+            onChange={(e) => setNacelleEntryId(e.target.value)}
+          />
+          <button onClick={getContent}>Submit</button>
+        </section>
+  
+        <section>
+          <h2>Response</h2>
+          <button
+            onClick={() => {
+              navigator.clipboard.writeText(
+                JSON.stringify(jsonResponse, null, 2)
+              );
+            }}
+          >
+            Copy JSON
+          </button>
+          <pre>{JSON.stringify(jsonResponse, null, 2)}</pre>
+        </section>
+      </div>
+    )  
+}
+
+export default ContentByHandleV2SDK;
