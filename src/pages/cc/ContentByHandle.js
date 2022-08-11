71c04fe5fc54f2870a76ad7a16e8ddc3d38e6c27
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-08-11 10:24:20 -0400

    extended Response component

--- src/pages/cc/ContentByHandle.js
+++ src/pages/cc/ContentByHandle.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import NacelleClient from "@nacelle/client-js-sdk";
 import createCompatibilityConnector from "@nacelle/compatibility-connector";
+import Response from "../../layouts/Response";
 
 function ContentByHandle() {
   const [spaceId, setSpaceId] = useState("");
@@ -31,59 +32,45 @@ function ContentByHandle() {
 
   return (
     <>
-      <div className="App">
-        <h1>[CC] Get Content by Handle</h1>
-        <section>
-          <h2>Settings</h2>
-          <label htmlFor="spaceId">spaceId</label>
-          <input
-            type="text"
-            id="spaceId"
-            name="spaceId"
-            value={spaceId}
-            onChange={(e) => setSpaceId(e.target.value)}
-          />
-          <label htmlFor="token">token</label>
-          <input
-            type="text"
-            id="token"
-            name="token"
-            value={token}
-            onChange={(e) => setToken(e.target.value)}
-          />
-          <label htmlFor="handle">handle</label>
-          <input
-            type="text"
-            id="handle"
-            name="handle"
-            value={handle}
-            onChange={(e) => setHandle(e.target.value)}
-          />
-          <label htmlFor="nacelleEntryId">nacelleEntryId (do not use)</label>
-          <input
-            type="text"
-            id="nacelleEntryId"
-            name="nacelleEntryId"
-            value={nacelleEntryId}
-            onChange={(e) => setNacelleEntryId(e.target.value)}
-          />
-          <button onClick={getContent}>Submit</button>
-        </section>
+      <h1>[CC] Get Content by Handle</h1>
+      <section>
+        <h2>Settings</h2>
+        <label htmlFor="spaceId">spaceId</label>
+        <input
+          type="text"
+          id="spaceId"
+          name="spaceId"
+          value={spaceId}
+          onChange={(e) => setSpaceId(e.target.value)}
+        />
+        <label htmlFor="token">token</label>
+        <input
+          type="text"
+          id="token"
+          name="token"
+          value={token}
+          onChange={(e) => setToken(e.target.value)}
+        />
+        <label htmlFor="handle">handle</label>
+        <input
+          type="text"
+          id="handle"
+          name="handle"
+          value={handle}
+          onChange={(e) => setHandle(e.target.value)}
+        />
+        <label htmlFor="nacelleEntryId">nacelleEntryId (do not use)</label>
+        <input
+          type="text"
+          id="nacelleEntryId"
+          name="nacelleEntryId"
+          value={nacelleEntryId}
+          onChange={(e) => setNacelleEntryId(e.target.value)}
+        />
+        <button onClick={getContent}>Submit</button>
+      </section>
 
-        <section>
-          <h2>Response</h2>
-          <button
-            onClick={() => {
-              navigator.clipboard.writeText(
-                JSON.stringify(jsonResponse, null, 2)
-              );
-            }}
-          >
-            Copy JSON
-          </button>
-          <pre>{JSON.stringify(jsonResponse, null, 2)}</pre>
-        </section>
-      </div>
+      <Response jsonResponse={jsonResponse}></Response>
     </>
   );
 }
