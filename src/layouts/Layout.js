321802116642dbc6cd0c2cbf43c9f3bf2e93da68
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-08-12 11:06:33 -0400

    Layout component now works

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,5 +1,67 @@
+import { useState } from "react";
+import Response from "./Response";
+
 function Layout(props) {
-  return <></>;
+  const [spaceId, setSpaceId] = useState("");
+  const [token, setToken] = useState("");
+  const [handle, setHandle] = useState("");
+  const [nacelleEntryId, setNacelleEntryId] = useState("");
+  const [jsonResponse, setJsonResponse] = useState({
+    msg: "waiting on credentials",
+  });
+
+  return (
+    <>
+      <h1>{[props.title]}</h1>
+      <section>
+        <h2>Settings</h2>
+        <label htmlFor="spaceId">spaceId</label>
+        <input
+          type="text"
+          id="spaceId"
+          name="spaceId"
+          value={spaceId}
+          onChange={(e) => setSpaceId(e.target.value)}
+        />
+        <label htmlFor="token">token</label>
+        <input
+          type="text"
+          id="token"
+          name="token"
+          value={token}
+          onChange={(e) => setToken(e.target.value)}
+        />
+        <label htmlFor="handle">handle</label>
+        <input
+          type="text"
+          id="handle"
+          name="handle"
+          value={handle}
+          onChange={(e) => setHandle(e.target.value)}
+        />
+        <label htmlFor="nacelleEntryId">nacelleEntryId (do not use)</label>
+        <input
+          type="text"
+          id="nacelleEntryId"
+          name="nacelleEntryId"
+          value={nacelleEntryId}
+          onChange={(e) => setNacelleEntryId(e.target.value)}
+        />
+
+        <button
+          onClick={() =>
+            props.requestFunc(spaceId, token, handle).then((resp) => {
+              setJsonResponse(resp);
+            })
+          }
+        >
+          Submit
+        </button>
+      </section>
+
+      <Response jsonResponse={jsonResponse}></Response>
+    </>
+  );
 }
 
 export default Layout;
--- src/pages/cc/ContentByHandle.js
+++ src/pages/cc/ContentByHandle.js
@@ -1,18 +1,9 @@
-import { useState } from "react";
 import NacelleClient from "@nacelle/client-js-sdk";
 import createCompatibilityConnector from "@nacelle/compatibility-connector";
-import Response from "../../layouts/Response";
+import Layout from "../../layouts/Layout";
 
 function ContentByHandle() {
-  const [spaceId, setSpaceId] = useState("");
-  const [token, setToken] = useState("");
-  const [handle, setHandle] = useState("");
-  const [nacelleEntryId, setNacelleEntryId] = useState("");
-  const [jsonResponse, setJsonResponse] = useState({
-    msg: "waiting on credentials",
-  });
-
-  async function getContent() {
+  async function getContent(spaceId, token, handle) {
     const compatibilityConnector = new createCompatibilityConnector({
       endpoint: `https://storefront.api.nacelle.com/graphql/v1/spaces/${spaceId}`,
       token: token,
@@ -27,50 +18,16 @@ function ContentByHandle() {
       handle: handle,
     });
 
-    setJsonResponse(content);
+    return content;
+  }
+
+  function hello(sId) {
+    console.log(`hi ${sId}`);
   }
 
   return (
     <>
-      <h1>[CC] Get Content by Handle</h1>
-      <section>
-        <h2>Settings</h2>
-        <label htmlFor="spaceId">spaceId</label>
-        <input
-          type="text"
-          id="spaceId"
-          name="spaceId"
-          value={spaceId}
-          onChange={(e) => setSpaceId(e.target.value)}
-        />
-        <label htmlFor="token">token</label>
-        <input
-          type="text"
-          id="token"
-          name="token"
-          value={token}
-          onChange={(e) => setToken(e.target.value)}
-        />
-        <label htmlFor="handle">handle</label>
-        <input
-          type="text"
-          id="handle"
-          name="handle"
-          value={handle}
-          onChange={(e) => setHandle(e.target.value)}
-        />
-        <label htmlFor="nacelleEntryId">nacelleEntryId (do not use)</label>
-        <input
-          type="text"
-          id="nacelleEntryId"
-          name="nacelleEntryId"
-          value={nacelleEntryId}
-          onChange={(e) => setNacelleEntryId(e.target.value)}
-        />
-        <button onClick={getContent}>Submit</button>
-      </section>
-
-      <Response jsonResponse={jsonResponse}></Response>
+      <Layout title="[CC] Get Content by Handle" requestFunc={getContent}></Layout>
     </>
   );
 }
