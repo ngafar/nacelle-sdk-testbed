180e5482dde7807acecc3b7448c7b6dd2540b50a
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-08-12 12:49:47 -0400

    added AllCollections

--- src/pages/v2/AllCollections.js
+++ src/pages/v2/AllCollections.js
@@ -1,14 +1,14 @@
 import Storefront from "@nacelle/storefront-sdk";
 import Layout from "../../layouts/Layout";
 
-function AllProducts() {
+function AllCollections() {
   async function getContent(spaceId, token, handle) {
     const client = Storefront({
       storefrontEndpoint: `https://storefront.api.nacelle.com/graphql/v1/spaces/${spaceId}`,
       token: token,
     });
 
-    const content = await client.products({});
+    const content = await client.productCollections({});
 
     console.log(content);
     return content;
@@ -16,9 +16,9 @@ function AllProducts() {
 
   return (
     <>
-      <Layout title="[V2] Get All Products" requestFunc={getContent}></Layout>
+      <Layout title="[V2] Get All Collections" requestFunc={getContent}></Layout>
     </>
   );
 }
 
-export default AllProducts;
+export default AllCollections;
--- src/pages/v2/_routes.js
+++ src/pages/v2/_routes.js
@@ -1,10 +1,12 @@
 import AllProducts from "./AllProducts";
+import AllCollections from "./AllCollections";
 import AllContent from "./AllContent";
 import ProductByHandle from './ProductByHandle'
 import ContentByHandle from "./ContentByHandle";
 
 const v2_routes_group = [
     {path: '/v2/all-products', component: AllProducts},
+    {path: '/v2/all-collections', component: AllCollections},
     {path: '/v2/all-content', component: AllContent},
     {path: '/v2/product-by-handle', component: ProductByHandle},
     {path: '/v2/content-by-handle', component: ContentByHandle}
