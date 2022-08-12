65054311d5e472b4557e0bf7d41d8ac06cb12af7
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-08-12 12:39:27 -0400

    added AllProducts

---
+++ src/pages/v2/AllProducts.js
@@ -0,0 +1,24 @@
+import Storefront from "@nacelle/storefront-sdk";
+import Layout from "../../layouts/Layout";
+
+function AllContent() {
+  async function getContent(spaceId, token, handle) {
+    const client = Storefront({
+      storefrontEndpoint: `https://storefront.api.nacelle.com/graphql/v1/spaces/${spaceId}`,
+      token: token,
+    });
+
+    const content = await client.products({});
+
+    console.log(content);
+    return content;
+  }
+
+  return (
+    <>
+      <Layout title="[V2] Get All Products" requestFunc={getContent}></Layout>
+    </>
+  );
+}
+
+export default AllContent;
--- src/pages/v2/_routes.js
+++ src/pages/v2/_routes.js
@@ -1,8 +1,10 @@
+import AllProducts from "./AllProducts";
 import AllContent from "./AllContent";
 import ProductByHandle from './ProductByHandle'
 import ContentByHandle from "./ContentByHandle";
 
 const v2_routes_group = [
+    {path: '/v2/all-products', component: AllProducts},
     {path: '/v2/all-content', component: AllContent},
     {path: '/v2/product-by-handle', component: ProductByHandle},
     {path: '/v2/content-by-handle', component: ContentByHandle}
