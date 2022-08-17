3d94477965a0cfee85f0a6e0275131f57e62d4df
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-08-17 13:32:59 -0400

    cleaned up cc routes

--- src/pages/cc/_routes.js
+++ src/pages/cc/_routes.js
@@ -1,23 +1,9 @@
 import ProductByHandle from './ProductByHandle'
 import ContentByHandle from "./ContentByHandle";
 
-/*
-import AllProducts from "./AllProducts";
-import AllCollections from "./AllCollections";
-import AllContent from "./AllContent";
-import CollectionByHandle from './CollectionByHandle'
-*/
-
 const cc_routes_group = [
     {path: '/cc/product-by-handle', component: ProductByHandle, name: "Get Product by Handle"},
     {path: '/cc/content-by-handle', component: ContentByHandle, name: "Get Content by Handle"}
-    /*
-    {path: '/cc/all-products', component: AllProducts, name: "Get All Products"},
-    {path: '/cc/all-collections', component: AllCollections, name: "Get All Collections"},
-    {path: '/cc/all-content', component: AllContent, name: "Get All Content"},
-
-    {path: '/cc/collection-by-handle', component: CollectionByHandle, name: "Get Collection by Handle"},
-    */
 ]
 
 export default cc_routes_group
