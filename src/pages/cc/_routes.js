cb24b7fd55a6b62497dea1a3048932d460365b4e
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-09-30 13:47:26 -0400

    added cc/article-by-handle

--- src/pages/cc/_routes.js
+++ src/pages/cc/_routes.js
@@ -1,10 +1,12 @@
 import AllContent from './AllContent'
 import ProductByHandle from './ProductByHandle'
+import ArticleByHandle from "./ArticleByHandle";
 import ContentByHandle from "./ContentByHandle";
 
 const cc_routes_group = [
     {path: '/cc/all-content', component: AllContent, name: "Get All Content"},
     {path: '/cc/product-by-handle', component: ProductByHandle, name: "Get Product by Handle"},
+    {path: '/cc/article-by-handle', component: ArticleByHandle, name: "Get Article by Handle"},
     {path: '/cc/content-by-handle', component: ContentByHandle, name: "Get Content by Handle"}
 ]
 
