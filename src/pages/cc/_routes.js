1a5d213d9cde7bad72924b1a0567d96a5fed761d
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-09-30 13:50:32 -0400

    Redorder paths

--- src/pages/cc/_routes.js
+++ src/pages/cc/_routes.js
@@ -1,13 +1,15 @@
+import AllArticles from './AllArticles'
 import AllContent from './AllContent'
-import ProductByHandle from './ProductByHandle'
 import ArticleByHandle from "./ArticleByHandle";
 import ContentByHandle from "./ContentByHandle";
+import ProductByHandle from './ProductByHandle'
 
 const cc_routes_group = [
+    {path: '/cc/all-articles', component: AllArticles, name: "Get All Articles"},
     {path: '/cc/all-content', component: AllContent, name: "Get All Content"},
-    {path: '/cc/product-by-handle', component: ProductByHandle, name: "Get Product by Handle"},
     {path: '/cc/article-by-handle', component: ArticleByHandle, name: "Get Article by Handle"},
     {path: '/cc/content-by-handle', component: ContentByHandle, name: "Get Content by Handle"}
+    {path: '/cc/product-by-handle', component: ProductByHandle, name: "Get Product by Handle"},
 ]
 
 export default cc_routes_group
