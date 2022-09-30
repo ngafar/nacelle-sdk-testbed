06e36dd0b55ea11c6605e6de6c3f2ec31d87a260
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-09-30 14:22:48 -0400

    Added methods

--- src/pages/cc/_routes.js
+++ src/pages/cc/_routes.js
@@ -1,22 +1,22 @@
-import AllArticles from './AllArticles'
-import AllContent from './AllContent'
-import AllPages from './AllPages'
+import AllArticles from './AllArticles';
+import AllContent from './AllContent';
+import AllPages from './AllPages';
 import ArticleByHandle from "./ArticleByHandle";
-import BlogByHandle from './BlogByHandle'
+import BlogByHandle from './BlogByHandle';
 import ContentByHandle from "./ContentByHandle";
-import PageByHandle from './PageByHandle'
-import ProductByHandle from './ProductByHandle'
+import PageByHandle from './PageByHandle';
+import ProductByHandle from './ProductByHandle';
 
 const cc_routes_group = [
-    {path: '/cc/all-articles', component: AllArticles, name: "Get All Articles"},
-    {path: '/cc/all-content', component: AllContent, name: "Get All Content"},
-    {path: '/cc/all-pages', component: AllPages, name: "Get All Pages"},
+    {path: '/cc/all-articles', component: AllArticles, name: "Get All Articles (.articles)"},
+    {path: '/cc/all-content', component: AllContent, name: "Get All Content (.allContent)"},
+    {path: '/cc/all-pages', component: AllPages, name: "Get All Pages (.pages)"},
 
-    {path: '/cc/article-by-handle', component: ArticleByHandle, name: "Get Article by Handle"},
-    {path: '/cc/blog-by-handle', component: BlogByHandle, name: "Get Blog (Entry) by Handle"},
-    {path: '/cc/content-by-handle', component: ContentByHandle, name: "Get Content by Handle"},
-    {path: '/cc/page-by-handle', component: PageByHandle, name: "Get Page by Handle"},
-    {path: '/cc/product-by-handle', component: ProductByHandle, name: "Get Product by Handle"},
+    {path: '/cc/article-by-handle', component: ArticleByHandle, name: "Get Article by Handle (.article)"},
+    {path: '/cc/blog-by-handle', component: BlogByHandle, name: "Get Blog (Entry) by Handle (.blog)"},
+    {path: '/cc/content-by-handle', component: ContentByHandle, name: "Get Content by Handle (.content)"},
+    {path: '/cc/page-by-handle', component: PageByHandle, name: "Get Page by Handle (.page)"},
+    {path: '/cc/product-by-handle', component: ProductByHandle, name: "Get Product by Handle (.product)"},
 ]
 
 export default cc_routes_group
