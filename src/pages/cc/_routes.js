ef6c76a31d72afd187da2110103a7b7d5b8ebbce
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-09-30 13:50:49 -0400

    Missing comma

--- src/pages/cc/_routes.js
+++ src/pages/cc/_routes.js
@@ -8,7 +8,7 @@ const cc_routes_group = [
     {path: '/cc/all-articles', component: AllArticles, name: "Get All Articles"},
     {path: '/cc/all-content', component: AllContent, name: "Get All Content"},
     {path: '/cc/article-by-handle', component: ArticleByHandle, name: "Get Article by Handle"},
-    {path: '/cc/content-by-handle', component: ContentByHandle, name: "Get Content by Handle"}
+    {path: '/cc/content-by-handle', component: ContentByHandle, name: "Get Content by Handle"},
     {path: '/cc/product-by-handle', component: ProductByHandle, name: "Get Product by Handle"},
 ]
 
