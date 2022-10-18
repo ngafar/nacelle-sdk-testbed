22f5efc0319a90e79eb3ecf952d3e078a207a416
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-10-18 16:36:51 -0400

    Added .

--- src/pages/cc/_routes.js
+++ src/pages/cc/_routes.js
@@ -8,15 +8,15 @@ import PageByHandle from './PageByHandle';
 import ProductByHandle from './ProductByHandle';
 
 const cc_routes_group = [
-    {path: '/cc/all-articles', component: AllArticles, name: "articles"},
-    {path: '/cc/all-content', component: AllContent, name: "allContent"},
-    {path: '/cc/all-pages', component: AllPages, name: "pages"},
+    {path: '/cc/all-articles', component: AllArticles, name: ".articles"},
+    {path: '/cc/all-content', component: AllContent, name: ".allContent"},
+    {path: '/cc/all-pages', component: AllPages, name: ".pages"},
 
-    {path: '/cc/article-by-handle', component: ArticleByHandle, name: "article"},
-    {path: '/cc/blog-by-handle', component: BlogByHandle, name: "blog"},
-    {path: '/cc/content-by-handle', component: ContentByHandle, name: "content"},
-    {path: '/cc/page-by-handle', component: PageByHandle, name: "page"},
-    {path: '/cc/product-by-handle', component: ProductByHandle, name: "product"},
+    {path: '/cc/article-by-handle', component: ArticleByHandle, name: ".article"},
+    {path: '/cc/blog-by-handle', component: BlogByHandle, name: ".blog"},
+    {path: '/cc/content-by-handle', component: ContentByHandle, name: ".content"},
+    {path: '/cc/page-by-handle', component: PageByHandle, name: ".page"},
+    {path: '/cc/product-by-handle', component: ProductByHandle, name: ".product"},
 ]
 
 export default cc_routes_group
